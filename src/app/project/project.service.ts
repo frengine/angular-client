690650da245623bf10f04bf11aa8ab625bdbb934
Maarten Oosting <mail@maartenoosting.nl>
2019-06-21 14:25:21 +0200

    Update urls

--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -1,28 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 export interface Author {
-	id: number;
-	name: string;
+  id: number;
+  name: string;
 }
 
 export interface Project {
-	id: number;
-	name: string;
-	author: Author;
-	modtime: number;
-	created: number;
+  id: number;
+  name: string;
+  author: Author;
+  modtime: number;
+  created: number;
 }
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class ProjectService {
-	constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-	getAllURL = "/projects";
+  getAllURL = '/api/projects';
 
-	getAll() {
-		return this.http.get<Project[]>(this.getAllURL);
-	}
+  getAll() {
+    return this.http.get<Project[]>(this.getAllURL);
+  }
 }
