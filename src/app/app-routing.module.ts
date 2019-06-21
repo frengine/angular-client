66ccb67b72d2cef893cc3def175e9d65d2bf70b8
Maarten Oosting <mail@maartenoosting.nl>
2019-06-21 14:59:26 +0200

    Update homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { ProjectComponent } from './project/project.component';
 import {ShaderTestComponent} from './shader-test/shader-test.component';
 import {RegisterComponent} from './login/register.component';
+import {NewProjectComponent} from './project/new/newproject.component';
 
 const routes: Routes = [
   { path: '', component: ProjectComponent },
+  { path: 'project/new', component: NewProjectComponent },
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'shader', component: ShaderTestComponent}
