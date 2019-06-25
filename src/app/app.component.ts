d230a10722499ef9f3d49559ee437941cf2fd763
hilkojj <hilkojj@outlook.com>
2019-06-25 13:06:59 +0200

    reload application when update available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-client';
+  constructor(
+    swUpdate: SwUpdate
+  ) {
+    swUpdate.available.subscribe(() => window.location.reload())
+    swUpdate.isEnabled && swUpdate.checkForUpdate()
+  }
 }
